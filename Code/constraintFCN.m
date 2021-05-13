0b74c7f2f91862d188515a4ab94f71d079aacd2f
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2021-05-13 15:28:39 +0200

    Create constraintFCN.m

---
+++ Code/constraintFCN.m
@@ -0,0 +1,51 @@
+function [c, ceq] = ConstraintFCN(u,x,N,p,select_model)
+%% Constraint function of nonlinear MPC for Lotka-Volterra system
+%
+% Inputs:
+%   u:      optimization variable, from time k to time k+N-1 
+%   x:      current state at time k
+%   Ts:     controller sample time
+%   N:      prediction horizon
+%
+% Output:
+%   c:      inequality constraints applied across prediction horizon
+%   ceq:    equality constraints (empty)
+%
+
+%% Nonlinear MPC design parameters
+% Infectious population <= zMax
+zMax = p.Imax;
+
+%% Integrate system
+if strcmp(select_model,'DMD')
+    [xk,~] = lsim(p.sys,[u',0],[0:N].*p.dt,x);
+    xk = xk(2:end,:); xk = xk'; 
+elseif strcmp(select_model,'SINDy')
+    Ns = size(x,1);
+    xk = zeros(Ns,N+1); xk(:,1) = x;
+    for ct=1:N
+        % Obtain plant state at next prediction step.
+        xk(:,ct+1) = rk4u(@sparseGalerkinControl_Discrete,xk(:,ct),u(ct),p.dt,1,[],p);
+    end
+    xk = xk(:,2:N+1);
+end
+
+
+%% Inequality constraints calculation
+c = zeros(N,1);
+% Apply N population size constraints across prediction horizon, from time
+% k+1 to k+N
+uk = u(1);
+for ct=1:N
+    % -z + zMin < 0 % lower bound
+%     c(ct) = -xk(2,ct)+zMin; %c(2*ct-1)
+    % z - zMax < 0 % upper bound
+    c(ct) = xk(3,ct)-zMax; % max 100 infected people
+    % update  input for next step
+    if ct<N
+        uk = u(ct+1);
+    end
+end
+%% No equality constraints
+ceq = [];
+
