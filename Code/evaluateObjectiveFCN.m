cde7af3e255d24bf07c45b852fb45dd132a83bc2
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2021-05-13 15:28:47 +0200

    Create evaluateObjectiveFCN.m

---
+++ Code/evaluateObjectiveFCN.m
@@ -0,0 +1,38 @@
+function J = evalObjectiveFCN(u,x,xref,Q,R,Ru,p)
+% [J,Js,Ju]
+%% Cost function of nonlinear MPC for SEIR model
+%
+% Inputs:
+%   u:      optimization variable, from time k to time k+N-1 
+%   x:      current state at time k
+%   Ts:     controller sample time
+%   N:      prediction horizon
+%   xref:   state references, constant from time k+1 to k+N
+%   u0:     previous controller output at time k-1
+%
+% Output:
+%   J:      objective function cost
+%
+
+%% Nonlinear MPC design parameters
+
+%% Cost Calculation
+% Set initial cost and input
+N = size(x,2);
+J = zeros(N,1);
+u0 = 0;
+
+% Loop through each time step
+for ct=1:N
+    
+    % Accumulate state tracking cost from x(k+1) to x(k+N)
+    J(ct) = (x(:,ct)-xref(:,ct))'*Q*(x(:,ct)-xref(:,ct));
+    
+    % Accumulate MV rate of change cost from u(k) to u(k+N-1)
+    if ct==1
+        J(ct) = J(ct) + (u(:,ct)-u0)'*R*(u(:,ct)-u0) + (p.beta0-u(:,ct))'*Ru*(p.beta0-u(:,ct));
+    else
+        J(ct) = J(ct) + (u(:,ct)-u(:,ct-1))'*R*(u(:,ct)-u(:,ct-1)) + (p.beta0-u(:,ct))'*Ru*(p.beta0-u(:,ct));
+    end
+end
+
