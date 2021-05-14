4c8f16d8a9404756b8b46d8b5929b2be5de98f70
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2021-05-14 11:42:46 +0200

    Update main.m

--- Code/main.m
+++ Code/main.m
@@ -3,7 +3,7 @@
 % Tutorial using SINDy or DMD to identify a SEIR model
 % Identified model used in MPC infectious disease control
 %
-% main parameters to initialize before running: 
+% Main parameters to initialize before running: 
 % line 16: select model for system identification -> DMD or SINDy
 % line 20: choose to run MPC with or without constraint
 %
@@ -13,21 +13,21 @@ clear all
 close all
 clc
 
-% select DMD or SINDy model for system identification
-select_model = 'DMD';
-% select_model = 'SINDy';
+% Select DMD or SINDy model for system identification
+% select_model = 'DMD';
+select_model = 'SINDy';
 
-% choose to run MPC with or without constraint
-constrON = 1; % constraint on
-% constrON = 0; % constraint off
+% Choose to run MPC with or without constraint
+constrON = 1; % Constraint on
+% constrON = 0; % Constraint off
 
 
 %% Initialize problem
 
 % Parameters of SEIR model
-p.beta0	= 0.5;      % transmitting rate 
-p.gamma	= 0.2;      % recovery rate 
-p.k     = 0.2;      % incubation rate: 5 days
+p.beta0	= 0.5;      % Transmitting rate 
+p.gamma	= 0.2;      % Recovery rate 
+p.k     = 0.2;      % Incubation rate
 
 % Initial conditions
 S0      = 0.996;    % Initial susceptible population
@@ -53,18 +53,18 @@ T_opt   = 300; % MPC phase, number of days
 
 
 %% Define PRBS forcing function to excite system for model identification
-Nic     = 2;         % number of different PRBS signals: 1 testing and 1 training
-taulim  = [14 28];   % update training and testing intervention each 14 to 28 days
-states  = 0.2:0.1:1; % interventions vary between u(t) = [0.3, 1] with 0.1 steps
-Nswitch = 1000;      % number of switches in prbs function
-rng(1,'twister');    % control random number generator
-seed    = randi(Nic,Nic,1); % vary actuation in each trajectory
+Nic     = 2;         % Number of different PRBS signals: 1 testing and 1 training
+taulim  = [14 28];   % Update training and testing intervention each 14 to 28 days
+states  = 0.2:0.1:1; % Interventions vary between u(t) = [0.3, 1] with 0.1 steps
+Nswitch = 1000;      % Number of switches in prbs function
+rng(1,'twister');    % Control random number generator
+seed    = randi(Nic,Nic,1); % Vary actuation in each trajectory
 forcing = @(x,t,seedval) p.beta0*prbs(taulim, Nswitch, states, t,0,seedval);
 
 SeedTrain = 1;  % PRBS signal 1 for training
 SeedTest = 2;   % PRBS signal 2 for testing
 
-% define training and testing forcing u(t)
+% Define training and testing forcing u(t)
 tspanTrain  = 0:dt:T_train; % Time span training
 for i = 1:length(tspanTrain)
     uTrain(i,:) = forcing(0,tspanTrain(i),seed(SeedTrain));
@@ -77,58 +77,63 @@ end
 
 %% Generate Data
 % Integrate forced SEIR system
-options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,n));
-[~,xTrain]=ode45(@(t,x) SEIR(t,x,forcing(x,t,seed(SeedTrain)),p),tspanTrain,x0,options);
+options = odeset('RelTol',1e-12,'AbsTol',1e-12*ones(1,n)); % Define ode options
+[~,xTrain]=ode45(@(t,x) SEIR(t,x,forcing(x,t,seed(SeedTrain)),p),tspanTrain,x0,options); % Integrate SEIR model using ode45
 
-% plot SEIR training data
+% Plot SEIR training data
 figure;
 plot(tspanTrain,xTrain,'LineWidth',1.5)
 set(gca,'FontSize',14)
 title('Training data')
 xlabel('Time')
-ylabel('Compartment')
+ylabel('Population ratios (Compartments)')
 legend('Suscepticle','Exposed','Infected','Recovered')
 
 
 %% Compute true derivative
-eps = 0.0; % noise level
+eps = 0.0; % Noise level
 for i=1:length(xTrain)
-    dx(i,:) = SEIR(0,xTrain(i,:),uTrain(i),p);
+    dx(i,:) = SEIR(0,xTrain(i,:),uTrain(i),p); % Run SEIR model to compute derivatives
 end
-dx(:,n+1) = 0*dx(:,n);
-dx = dx + eps*randn(size(dx));
-xTrain = [xTrain uTrain];
+dx(:,n+1) = 0*dx(:,n);          % Add additional column of zeros for derivatives of u, needed in SINDy algorithm
+dx = dx + eps*randn(size(dx));  % Add measurement noise
+xTrain = [xTrain uTrain];       % Stack state and input measurements
 
 
 %% Model Identification
 if strcmp(select_model,'DMD')
-    [sysmodel_DMDc,U,Up] = DMDc(xTrain(:,1:4)',uTrain',dt);
+    [sysmodel_DMDc,U,Up] = DMDc(xTrain(:,1:4)',uTrain',dt); % Identify DMDc model
 elseif strcmp(select_model,'SINDy')
-    clear Theta Xi
-    Theta = poolData(xTrain,(n+1),polyorder,usesine); % generate library
-    lambda = 1e-1;     % lambda is our sparsification knob.
-    Xi = sparsifyDynamics(Theta,dx,lambda,(n+1)); % run SINDy using sequential treshold least squares
-    poolDataLIST({'S','E','I','R','u'},Xi,(n+1),polyorder,usesine); % list library terms
+    Theta = poolData(xTrain,(n+1),polyorder,usesine); % Generate library
+    lambda = 1e-1;     % lambda is our sparsification hyperparameter.
+    Xi = sparsifyDynamics(Theta,dx,lambda,(n+1)); % Run SINDy using sequential treshold least squares
+    poolDataLIST({'S','E','I','R','u'},Xi,(n+1),polyorder,usesine); % List library terms
 end
 
 
 %% Run validation
-% true dynamics
+% True dynamics: integrate forced true SEIR model using ode45
 [~,xTrueTest]=ode45(@(t,x)SEIR(t,x,forcing(x,t,seed(SeedTest)),p),tspanTest,x0,options);
 
 % DMD or SINDy dynamics
 if strcmp(select_model,'DMD')
-    [xModelTrain,~] = lsim(sysmodel_DMDc,uTrain,tspanTrain,x0);
-    [xModelTest,~] = lsim(sysmodel_DMDc,uTest,tspanTest,x0);
+    % DMD dynamics: integrate DMD model using linear simulation tool lsim
+    [xModelTrain,~] = lsim(sysmodel_DMDc,uTrain,tspanTrain,x0); % Training
+    [xModelTest,~] = lsim(sysmodel_DMDc,uTest,tspanTest,x0); % Testing
 elseif strcmp(select_model,'SINDy')
-    [~,xModelTrain]=ode45(@(t,x)sparseGalerkinControl(t,x,forcing(x,t,seed(SeedTrain)),Xi(:,1:n),polyorder,usesine),tspanTrain,x0,options);
-    [~,xModelTest]=ode45(@(t,x)sparseGalerkinControl(t,x,forcing(x,t,seed(SeedTest)),Xi(:,1:n),polyorder,usesine),tspanTest,x0,options);
+    % SINDy dynamics: integrate SINDy model using ode45
+    % the SINDy ode with control is computed using the function sparseGalerkinControl
+    p.ahat = Xi(:,1:n);      % SINDy model terms
+    p.polyorder = polyorder; 
+    p.usesine = usesine;
+    [~,xModelTrain]=ode45(@(t,x)sparseGalerkinControl(t,x,forcing(x,t,seed(SeedTrain)),p),tspanTrain,x0,options); % Training
+    [~,xModelTest]=ode45(@(t,x)sparseGalerkinControl(t,x,forcing(x,t,seed(SeedTest)),p),tspanTest,x0,options); % Testing
 end
 
-% unforced true dynamics for comparison
+% Integrate unforced true SEIR dynamics for comparison
 [~,xUnforced]=ode45(@(t,x)SEIR(t,x,p.beta0,p),tspanTest,x0,options);   
 
-% plot model validation
+% Plot model comparison
 clear ph
 figure,box on,
 ccolors = get(gca,'colororder');
@@ -142,7 +147,7 @@ plot(tspanTest,xModelTest(:,3),'--','Color',ccolors(3,:)-[0 0.2 0.09],'LineWidth
 plot(tspanTest,xModelTest(:,4),'--','Color',ccolors(4,:)-[0.1 0.1 0.09],'LineWidth',2);
 ylim([0 1])
 xlabel('Time, days')
-ylabel('Population ratios')
+ylabel('Population ratios (Compartments)')
 set(gca,'LineWidth',1, 'FontSize',14)
 title(['Comparing identified ' select_model ' vs. true dynamics'])
 legend('Suscepticle','Exposed','Infected','Recovered',['Suscepticle_{',select_model,'}'],['Exposed_{',select_model,'}'],['Infected_{',select_model,'}'],['Recovered_{',select_model,'}'])
@@ -165,20 +170,18 @@ uopt0       = p.beta0;      % Set initial control input to zero
 LB          = 0.3*p.beta0*ones(Nvec,1);	% Lower bound of control input
 UB          = p.beta0*ones(Nvec,1);     % Upper bound of control input
 
-% Reference state, which shall be achieved: only deviation from xref1(3)
-% (infectious cases) is penalized
+% Reference state, which shall be achieved: only deviation from xref1(3) (infectious cases) is penalized
 xref1 = [0; 0; 0; 0];
 
-% MPC parameters for objective and constraint function
+% MPC parameters used in objective and constraint function
 if strcmp(select_model,'DMD')
     pMPC.sys = sysmodel_DMDc;
-    pMPC.Nxlim = size(xTrain,1);
 elseif strcmp(select_model,'SINDy')
     pMPC.ahat = Xi(:,1:n);
     pMPC.polyorder = polyorder;
     pMPC.usesine = usesine;
 end
-pMPC.dt = Ts;
+pMPC.Ts = Ts;
 pMPC.beta0 = p.beta0;
 pMPC.Imax = Imax;   
     
@@ -188,8 +191,8 @@ pMPC.Imax = Imax;
 options = optimoptions('fmincon','Display','none');%,'MaxIterations',100);
 
 % Start simulation
-x        = x0n;
-uopt     = uopt0.*ones(Nvec,1);
+x        = x0n;     % Initial state
+uopt     = uopt0.*ones(Nvec,1); % Initial control input
 xHistory = x;       % Stores state history
 uHistory = uopt(1); % Stores control history
 tHistory = 0;       % Stores time history
@@ -204,12 +207,12 @@ for ct = 1:(Duration/Ts)
             xref = xref1;
 
             % NMPC with full-state feedback
-            COSTFUN = @(u) objectiveFCN(u,x,Nvec,xref,uopt(1),pMPC,diag(Q),R,Ru,select_model);
+            COSTFUN = @(u) objectiveFCN(u,x,Nvec,xref,uopt(1),pMPC,diag(Q),R,Ru,select_model); % Cost function
             if constrON
-                CONSFUN = @(u) constraintFCN(u,x,Nvec,pMPC,select_model);
-                uopt = fmincon(COSTFUN,uopt,[],[],[],[],LB,UB,CONSFUN,options);
+                CONSFUN = @(u) constraintFCN(u,x,Nvec,pMPC,select_model); % Constraint function
+                uopt = fmincon(COSTFUN,uopt,[],[],[],[],LB,UB,CONSFUN,options); % Optimization with constraint
             else
-                uopt = fmincon(COSTFUN,uopt,[],[],[],[],LB,UB,[],options);
+                uopt = fmincon(COSTFUN,uopt,[],[],[],[],LB,UB,[],options); % Optimization without constraint
             end
         end
     else    % If control is off
@@ -218,11 +221,11 @@ for ct = 1:(Duration/Ts)
     end
 
     % Integrate system: Apply control & Step one timestep forward
-    x = rk4u(@SEIR,x,uopt(1),Ts/1,1,[],p); %10, 2
-    xHistory = [xHistory x];
-    uHistory = [uHistory uopt(1)];
-    tHistory = [tHistory tHistory(end)+Ts/1];
-    rHistory = [rHistory xref];
+    x = rk4u(@SEIR,x,uopt(1),Ts/1,1,[],p);      % Runge-Kutta scheme of order 4 for control system
+    xHistory = [xHistory x];                    % Store current state
+    uHistory = [uHistory uopt(1)];              % Store current control
+    tHistory = [tHistory tHistory(end)+Ts/1];   % Store current time
+    rHistory = [rHistory xref];                 % Store current reference
 end
 
 % Evaluate objective function
@@ -231,7 +234,7 @@ J = evaluateObjectiveFCN(uHistory,xHistory,rHistory,diag(Q),R,Ru,pMPC);
 
 %% Plot results
 
-% infectious cases
+% State: infectious cases I(t)
 figure
 plot(tspanTest,xUnforced(:,3),'r','LineWidth',2); hold on
 plot(tHistory,xHistory(3,:),'b','LineWidth',2);
@@ -241,7 +244,7 @@ ylabel('Infectious population')
 set(gca,'FontSize',14)
 legend('No control','MPC','Constraint')
 
-% control input
+% Control input u(t)
 figure
 plot(tHistory,p.beta0-uHistory,'b','LineWidth',2);
 xlabel('Time')
@@ -249,7 +252,7 @@ ylabel('Control')
 set(gca,'FontSize',14)
 legend('Control input u(t)')
 
-% cost
+% Cost J(t)
 figure
 plot(tHistory,J,'g','LineWidth',2)
 xlabel('Time')
@@ -259,14 +262,14 @@ legend('Cost J(t)')
 
 
 
-%% save results
-% training
+%% Save results
+% Training
 Results.tTrain = tspanTrain;
 Results.xTrueTrain = xTrain;
 Results.xModelTrain = xModelTrain;
 Results.uTrain = uTrain;
 
-% testing
+% Testing
 Results.tTest = tspanTest;
 Results.xTrueTest = xTrueTest;
 Results.xModelTest = xModelTest;
@@ -278,15 +281,15 @@ Results.x = xHistory;
 Results.u = uHistory;
 Results.J = J;
 
-% unforced
+% Unforced
 Results.tUnforced = tspanTest;
 Results.xUnforced = xUnforced;
 
-%save
+% Save
 save(['../Results/Results_SEIR_MPC_' select_model '_C' num2str(constrON) '.mat'],'Results')
 
 
-%% plot figure 3 from tutorial paper
-% run 4 cases (DMD and SINDy with and without constraint) and compare the results: 
+%% Plot figure 3 from tutorial paper
+% First, run 4 cases (DMD and SINDy with and without constraint) 
 plotFigures
 
