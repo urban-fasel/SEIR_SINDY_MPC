a0ec4c08af12acc0f9c7098df2287b0910daf2b3
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2021-05-14 11:42:52 +0200

    Update sparseGalerkinControl.m

--- Code/sparseGalerkinControl.m
+++ Code/sparseGalerkinControl.m
@@ -1,9 +1,14 @@
-function dy = sparseGalerkinControl(t,y,u,ahat,polyorder,usesine)
+% function dy = sparseGalerkinControl(t,y,u,ahat,polyorder,usesine)
+function dy = sparseGalerkinControl(t,y,u,p)
 % Copyright 2015, All Rights Reserved
 % Code by Steven L. Brunton
 % For Paper, "Discovering Governing Equations from Data: 
 %        Sparse Identification of Nonlinear Dynamical Systems"
 % by S. L. Brunton, J. L. Proctor, and J. N. Kutz
+
+polyorder = p.polyorder;
+usesine = p.usesine;
+ahat = p.ahat;
 
 yPool = poolData([y' u],length(y)+1,polyorder,usesine);
 dy = (yPool*ahat)';
