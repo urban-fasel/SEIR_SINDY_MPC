94b53a6c936f377e020800037f6b755cde8f619e
urban-fasel <60729896+urban-fasel@users.noreply.github.com>
2021-05-14 11:42:50 +0200

    Update objectiveFCN.m

--- Code/objectiveFCN.m
+++ Code/objectiveFCN.m
@@ -1,13 +1,14 @@
-function J = ObjectiveFCN(u,x,N,xref,u0,p,Q,R,Ru, select_model)
+function J = objectiveFCN(u,x,N,xref,u0,p,Q,R,Ru, select_model)
 %% Cost function of nonlinear MPC for Lotka-Volterra system
 %
 % Inputs:
 %   u:      optimization variable, from time k to time k+N-1
 %   x:      current state at time k
-%   Ts:     controller sample time
 %   N:      prediction horizon
 %   xref:   state references, constant from time k+1 to k+N
 %   u0:     previous controller output at time k-1
+%   p:      model parameters
+%   Q,R,Ru: MPC cost function weights      
 %
 % Output:
 %   J:      objective function cost
@@ -15,14 +16,14 @@ function J = ObjectiveFCN(u,x,N,xref,u0,p,Q,R,Ru, select_model)
 
 %% Integrate system
 if strcmp(select_model,'DMD')
-    [xk,~] = lsim(p.sys,[u' 0],[0:N].*p.dt,x);
+    [xk,~] = lsim(p.sys,[u' 0],[0:N].*p.Ts,x);
     xk = xk(2:end,:); xk = xk';
 elseif strcmp(select_model,'SINDy')
     Ns = size(x,1);
     xk = zeros(Ns,N+1); xk(:,1) = x;
     for ct=1:N
         % Obtain plant state at next prediction step.
-        xk(:,ct+1) = rk4u(@sparseGalerkinControl_Discrete,xk(:,ct),u(ct),p.dt,1,[],p);
+        xk(:,ct+1) = rk4u(@sparseGalerkinControl,xk(:,ct),u(ct),p.Ts,1,[],p);
     end
     xk = xk(:,2:N+1);
 end
@@ -35,7 +36,6 @@ J = 0;
 for ct=1:N
     % Obtain plant state at next prediction step
     xk1 = xk(:,ct);
-    
     % Accumulate state tracking cost from x(k+1) to x(k+N)
     J = J + (xk1-xref)'*Q*(xk1-xref);
     % Accumulate MV rate of change cost from u(k) to u(k+N-1)
